06156a93d3f76a79241beebe7e8d715d4e73d087
Caio-Felipe-de-Carvalho <caiofelipe2909@gmail.com>
2025-03-15 19:26:49 -0300

    quarto-commit

--- script.js
+++ script.js
@@ -23,3 +23,21 @@ function addHoverEffect(selector, color) {
 Object.entries(hoverColors).forEach(([selector, color]) => {
     addHoverEffect(selector, color);
 });
+
+//dialog
+document.addEventListener("DOMContentLoaded", function() {
+    const menuIcon = document.getElementById("menu-icon");
+    const dialogBox = document.querySelector(".dialog-box");
+
+    menuIcon.addEventListener("click", function() {
+        if (window.innerWidth <= 768) {
+            dialogBox.classList.toggle("active");
+        }
+    });
+
+    document.addEventListener("click", function(event) {
+        if (window.innerWidth <= 768 && !menuIcon.contains(event.target) && !dialogBox.contains(event.target)) {
+            dialogBox.classList.remove("active");
+        }
+    });
+});
